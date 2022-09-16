e8f2733605d1a7c52f487362ed237a13b723d170
Sourav12100 <sourav.thakur@infostride.com>
2022-09-16 17:02:20 +0530

    TC_02

--- src/test/java/Pages/PageFactory.java
+++ src/test/java/Pages/PageFactory.java
@@ -4,8 +4,16 @@ import org.openqa.selenium.WebDriver;
 
 public class PageFactory {
     WebDriver driver;
+    private HomePage homePage;
 
     public PageFactory(WebDriver driver) {
         this.driver = driver;
     }
+    public HomePage getHomePage() {
+        if (homePage == null) {
+            homePage = new HomePage(driver);
+        }
+        return homePage;
+    }
+
 }
